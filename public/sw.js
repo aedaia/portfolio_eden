62e37127405abef44f47bcae9cadd4b23bf1084f
Eden <curlysinc@gmail.com>
2021-11-05 16:17:55 +0000

    Revert "-"
    This reverts commit 9feee50824a9feed2b1ab2e1f382634ba7ad2082.

---
+++ public/sw.js
@@ -0,0 +1,105 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-362ca758863355dce635.js"
+  },
+  {
+    "url": "framework-acd7498685eeb36e39da.js"
+  },
+  {
+    "url": "app-9a27198632aadf6dfd5d.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-36dc033f967ae9fe9cda.js"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "fffedf6d4de7f12c4d46b55102012c99"
+  },
+  {
+    "url": "polyfill-0a58c215dfbd45900075.js"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "e11cabc2f851fa24b8b80e2229af1edd"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, workbox.strategies.cacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\page-data\/.*\/page-data\.json/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, workbox.strategies.staleWhileRevalidate(), 'GET');
+
+/* global importScripts, workbox, idbKeyval */
+
+importScripts(`idb-keyval-iife.min.js`)
+
+const { NavigationRoute } = workbox.routing
+
+const navigationRoute = new NavigationRoute(async ({ event }) => {
+  let { pathname } = new URL(event.request.url)
+  pathname = pathname.replace(new RegExp(`^`), ``)
+
+  // Check for resources + the app bundle
+  // The latter may not exist if the SW is updating to a new version
+  const resources = await idbKeyval.get(`resources:${pathname}`)
+  if (!resources || !(await caches.match(`/app-9a27198632aadf6dfd5d.js`))) {
+    return await fetch(event.request)
+  }
+
+  for (const resource of resources) {
+    // As soon as we detect a failed resource, fetch the entire page from
+    // network - that way we won't risk being in an inconsistent state with
+    // some parts of the page failing.
+    if (!(await caches.match(resource))) {
+      return await fetch(event.request)
+    }
+  }
+
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  return await caches.match(offlineShell)
+})
+
+workbox.routing.registerRoute(navigationRoute)
+
+const messageApi = {
+  setPathResources(event, { path, resources }) {
+    event.waitUntil(idbKeyval.set(`resources:${path}`, resources))
+  },
+
+  clearPathResources(event) {
+    event.waitUntil(idbKeyval.clear())
+  },
+}
+
+self.addEventListener(`message`, event => {
+  const { gatsbyApi } = event.data
+  if (gatsbyApi) messageApi[gatsbyApi](event, event.data)
+})
---
+++ src/pages/glow.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import Layout from 'components/layout';
+import Box from 'components/box';
+import Title from 'components/title';
+import { graphql } from 'gatsby';
+import ReactPlayer from 'react-player';
+import { Link } from 'gatsby';
+import Footer from 'components/footer';
+
+const Glow = () => (
+  <Layout>
+    <Box>
+      <Title as="h2" size="large">
+        {' '}
+        Glow{' '}
+      </Title>
+      <p>
+        A mental health support app, designed to provide support for teens and
+        young people. Combining CBT, breathing techniques and journalling,
+        support is provided by trained professional psychologists. All
+        illustrations by Eden Sinclair.
+        <br></br>
+        <span>12/19–04/20</span>
+      </p>
+      <br></br> <br></br> <br></br> <br></br>
+      </Box>
+    
+    <img src={require('../images/cae80-eden_sinclair_glow_pitch_page_01.jpg')} alt="hello" />
+    <img src={require('../images/sleep.jpg')} alt="hello" />
+    <img src={require('../images/mood.jpg')} alt="hello" />
+    <img src={require('../images/chat.jpg')} alt="hello" />
+    <img src={require('../images/breathe.jpg')} alt="hello" />
+    <img src={require('../images/mood2.jpg')} alt="hello" />
+    <br></br> <br></br> <br></br> <br></br>
+
+
+      <Box>
+      <Title as="h3" size="medium">
+        The process
+      </Title>
+    </Box>  
+    <iframe title="processFile" width="100%" height="500px" src="https://drive.google.com/file/d/1y7uWE_LGB2A6Ia2S0mLSG5A_rwLeRXce/preview"></iframe>
+    <br></br> <br></br> <br></br> <br></br>
+    <Box>
+    <Title as="h3" size="medium">
+        App walkthrough
+      </Title>
+    </Box>
+
+    <div className="player-wrapper">
+      <ReactPlayer
+        width="100%"
+        height="100vh"
+        url="https://youtu.be/BsvbXNDwvuE"
+        controls="true"
+      />
+    </div>
+   
+    <Footer />
+  </Layout>
+);
+
+Glow.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default Glow;
+
+export const query = graphql`
+  query glowQuery {
+    glowJson {
+      title
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`;
