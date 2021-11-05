cec2fee0fe71fb408e4484dc4da38ca4f4796d72
Eden <curlysinc@gmail.com>
2021-11-05 15:22:07 +0000

    Revert "Update timeline2.js"
    This reverts commit 8411a67c3d435751f3378921f9775eb7142afd10.

--- src/components/timeline2/timeline2.js
+++ src/components/timeline2/timeline2.js
@@ -6,44 +6,33 @@ const Timeline2 = () => (
     <div className="timeline">
       <div className="column">
         <div className="title">
-          <h1>
-            {' '}
-            Visual designer – IBM <br /> 11/20–present
-          </h1>
+          <h1> Visual designer – IBM <br /> 11/20–present</h1>
         </div>
         <div className="description">
           <p>
             {' '}
-            Working primarily within the Automation space leading
-            the visual design of a product and assisting with UX and UI development. {' '}
+            I am responsible for xyz and xyz. I also do UX and research as well
+            as react development.{' '}
           </p>
         </div>
       </div>
 
       <div className="column">
         <div className="title">
-          <h1>
-            {' '}
-            Industry lecturer – University of Reading <br /> 09/21–present
-          </h1>
+          <h1> Industry lecturer – University of Reading <br /> 09/21–present</h1>
         </div>
         <div className="description">
           <p>
             {' '}
-            I helped to run a second year app project, which involved teaching
-            design thinking practices, leading feedback sessions on project work
-            with small groups of students, as well as running small workshops on
-            storyboarding and other design techniques.{' '}
+            I am responsible for xyz and xyz. I also do UX and research as well
+            as react development.{' '}
           </p>
         </div>
       </div>
 
       <div className="column">
         <div className="title">
-          <h1>
-            {' '}
-            Editorial designer – Prior Park College <br /> 07/20–10/20
-          </h1>
+          <h1> Editorial designer – Prior Park College <br /> 07/20–10/20</h1>
         </div>
         <div className="description">
           <p>
@@ -56,10 +45,7 @@ const Timeline2 = () => (
 
       <div className="column">
         <div className="title">
-          <h1>
-            {' '}
-            Graphic designer – Autism Berkshire <br /> 07/20–10/20
-          </h1>
+          <h1> Graphic designer  – Autism Berkshire <br /> 07/20–10/20</h1>
         </div>
         <div className="description">
           <p>
@@ -72,10 +58,7 @@ const Timeline2 = () => (
 
       <div className="column">
         <div className="title">
-          <h1>
-            {' '}
-            Design intern – IBM <br /> 06/19–09/19
-          </h1>
+          <h1> Design intern – IBM <br /> 06/19–09/19</h1>
         </div>
         <div className="description">
           <p>
@@ -89,19 +72,22 @@ const Timeline2 = () => (
 
       <div className="column">
         <div className="title">
-          <h1>
-            {' '}
-            Design editor – The Spark Newspaper <br /> 01/18–01/19
-          </h1>
+          <h1> Design editor  – The Spark Newspaper <br /> 01/18–01/19</h1>
         </div>
         <div className="description">
           <p>
             {' '}
-            I managed a team of designers and lead the redesign of The Spark Newspaper. {' '}
+            I am responsible for xyz and xyz. I also do UX and research as well
+            as react development.{' '}
           </p>
         </div>
       </div>
+      
     </div>
+    
+   
+
+
   </Container>
 );
 
